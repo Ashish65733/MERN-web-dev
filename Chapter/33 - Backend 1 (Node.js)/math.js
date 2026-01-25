27d291fd4510e711ac3a45953ddc0a9c446812e8
Ashish65733 <ashishlrana007@gmail.com>
2026-01-25 20:00:52 +0530

    33. Backend (Node.js) Half done..

---
+++ Chapter/33 - Backend 1 (Node.js)/math.js
@@ -0,0 +1,13 @@
+const sum = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const g = 9.8;
+const pi = 3.14;
+
+let obj = {
+    sum: sum,
+    subtract: subtract,
+    g: g,
+    pi: pi
+};
+
+module.exports = obj;
