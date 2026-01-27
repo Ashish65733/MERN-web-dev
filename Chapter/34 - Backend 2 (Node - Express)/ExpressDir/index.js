1ac9aeb327b69427c4d999e9753b3123ae3b12ea
Ashish65733 <ashishlrana007@gmail.com>
2026-01-27 22:24:59 +0530

    completed 34 - BE 2 (Node : Express)

--- Chapter/34 - Backend 2 (Node - Express)/ExpressDir/index.js
+++ Chapter/34 - Backend 2 (Node - Express)/ExpressDir/index.js
@@ -3,8 +3,58 @@ const app = express();
 
 // console.log(app);
 
-let port = 3000;
+let port = 8080;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+
+// app.use((req,res) => {
+//     // console.log(req);
+//     console.log('Request received');
+//     // res.send('This is a basic response from the server.');
+//     // res.send({
+//     //     name : "apple",
+//     //     color : "red"
+//     // });
+//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Banana</li><li>Orange</li></ul>";
+//     res.send(code);
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello, i am root path");
+});
+
+app.get("/apple", (req, res) => {
+    res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+    res.send("you contacted orange path");
+});
+
+app.get("*path", (req, res) => {
+    res.send("this path does not exist on the server");
+});
+
+app.post("/", (req, res) => {
+    res.send("you requested using the POST method");
+});
+
+app.get("/:username/:id",(req,res) => {
+    // console.log("Params received");
+    // console.log(req.params);
+    let { username, id } = req.params;
+    let htmlStr = `<h1>Welcome to the page of @${username}</h1>`;
+    // res.send(`You requested the profile of ${username}.`);
+    res.send(htmlStr);
+});
+
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    if(!q) {
+        res.send("No search query provided");
+    }
+    res.send(`These are the results for: ${q}`);
 });
