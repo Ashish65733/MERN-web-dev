c69f779149e3fce267a0f8bec1c686b2af626729
Ashish65733 <ashishlrana007@gmail.com>
2026-01-29 23:00:09 +0530

    Complete BE-3 (Node : EJS)

--- Chapter/35 - Backend 3 (Node - EJS)/index.js
+++ Chapter/35 - Backend 3 (Node - EJS)/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, '/public/CSS')));
+app.use(express.static(path.join(__dirname, '/public/JS')));
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
@@ -25,4 +27,22 @@ app.get("/rolldice", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// app.get("/ig/:username",(req, res) => {
+//     let followers = ["Sadie_sink", "Ronaldo", "Mr.Beast", "Elon Musk"];
+//     let { username } = req.params;
+//     res.render("instagram", { username, followers });
+// });
+
+app.get("/ig/:username",(req, res) => {
+    let { username } = req.params;
+    let instaData = require("./data.json");
+    let data = instaData[username];
+
+    if(data){
+        res.render("instagram", { data });
+    }else{
+        res.render("error");
+    }
 });
